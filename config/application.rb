6c9fc54bf69db74cfea70bac9cdad6f3062d12dc
AlexKtf <alexandre.ktifa@gmail.com>
2014-11-24 13:43:42 +0100

    Let use FRENCH translation

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module Website
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :fr
 
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
